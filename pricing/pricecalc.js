f6731d4c02ef7ab1d496bbe49714e3155208b265
TheNoobiCat <127432361+TheNoobiCat@users.noreply.github.com>
2024-01-17 21:44:35 +0000

    make pricing calculator

--- pricing/pricecalc.js
+++ pricing/pricecalc.js
@@ -1,5 +1,14 @@
 function getPriceWindow(){
-    var url = 'quote.html';
+    //calc price based on choices
+    pagecount = parseInt( document.getElementById("pages").value)
+    sitetype = parseFloat(document.getElementById("sitetype").value)
+    console.log(pagecount)
+    console.log(sitetype)
+    price=((pagecount*sitetype*3.99)*1.4).toFixed(2)
+    deposit=(price*0.1).toFixed(2)
+
+
+    var url = 'quote.html?price='+price+"&deposit="+deposit;
     var windowName = 'MiniWindow';
     var windowFeatures = 'width=800,height=800';
 
@@ -11,7 +20,9 @@ function FillPrice(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     price = urlParams.get("price")
+    deposit = urlParams.get("deposit")
     document.getElementById("price").innerHTML = "£"+price
+    document.getElementById("depositamt").innerHTML="I may ask for a deposit of £"+deposit+" on this order. The deposit amount is included in the total above."
 }
 
 window.onload = function(){
